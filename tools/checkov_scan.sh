f8a42ada7569671669acdb15854985cba9c731dc
markov-eni <marc.delefosse2025@campus-eni.fr>
2026-01-28 14:24:25 +0100

    build(frontend): add Dockerfile with one stage building

--- tools/checkov_scan.sh
+++ tools/checkov_scan.sh
@@ -1,1 +1,2 @@
 checkov -d task-manager/backend/  --framework dockerfile
+checkov -d task-manager/frontend/  --framework dockerfile
--- tools/hadolint_scan.sh
+++ tools/hadolint_scan.sh
@@ -1,1 +1,2 @@
-hadolint --strict-labels --require-label version:semver task-manager/backend/Dockerfile
+hadolint --strict-labels --require-label version:semver task-manager/backend/
+hadolint --strict-labels --require-label version:semver task-manager/frontend/
