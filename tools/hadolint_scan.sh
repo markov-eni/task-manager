8396dffb99af1de91cd26312ff3ba8757250760e
markov-eni <marc.delefosse2025@campus-eni.fr>
2026-01-28 15:43:18 +0100

    build(frontend): add Dockerfile

--- tools/hadolint_scan.sh
+++ tools/hadolint_scan.sh
@@ -1,2 +1,2 @@
-hadolint --strict-labels --require-label version:semver task-manager/backend/
-hadolint --strict-labels --require-label version:semver task-manager/frontend/
+hadolint --strict-labels --require-label version:semver task-manager/backend/Dockerfile
+hadolint --strict-labels --require-label version:semver task-manager/frontend/Dockerfile
