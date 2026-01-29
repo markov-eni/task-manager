1b7a1204d273e6c90e47cf3df5232059f0a9d7b9
markov-eni <marc.delefosse2025@campus-eni.fr>
2026-01-29 11:10:42 +0100

    ci: add dynamic env variables in order to be able to deploy with ci

---
+++ deploy/local_setup_env.sh
@@ -0,0 +1,5 @@
+# Environment variables to deploy locally
+export SQLITE_DB_PATH=../task-manager/backend/taskmanager.db
+export BACKEND_IMAGE=markoveni/task-manager:backend-2e3a7e32
+export FRONTEND_IMAGE=markoveni/task-manager:frontend-2e3a7e32
+export ALLOWED_ORIGINS=http://127.0.0.1:8080|http://localhost:8080
